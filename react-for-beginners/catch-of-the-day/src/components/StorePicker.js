83122e979590dbae83f39270a35f8413690d67aa
usyyy <usmaanalii@hotmail.co.uk>
2017-09-20 17:05:39 +0100

    Videos Completed
    6. Creating our application layout with components
    7. Passing Dynamic data with props
    8. Stateless Functional Components
    9. Routing with React Router
    10. Helper and Utility Functions
    11. Working with React Events
    12. All About React Router

--- react-for-beginners/catch-of-the-day/src/components/StorePicker.js
+++ react-for-beginners/catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  
+  goToStore(event) {
+    // grab the text from the box
+    event.preventDefault();
+    console.log('You changed the URL');
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    // transition route
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
-        {/* Comment in JSX */}
+      <form className="store-selector" onSubmit={(event) => this.goToStore(event)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
