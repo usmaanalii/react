4a1a425952a783ec5b11d9a344ad0d82726241da
usyyy <usmaanalii@hotmail.co.uk>
2017-11-12 16:59:29 +0000

    Completed steps 1-7 of the react-router tutorial

---
+++ react-routing/nader-dabit-tutorial/app/app.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, IndexLink } from 'react-router';
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = (props) => (
+  <div>
+    <br/>
+    <Link to='/address'>Twitter Feed</Link>&nbsp;
+    <Link to='/address/instagram'>Instagram Feed</Link>
+    <h1>We are located at 555 Jackson St.</h1>
+    {props.children}
+  </div>
+)
+
+const Instagram = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
+
+const NotFound = () => <h1>404.. This page is not found!</h1>
+
+const Nav = () => (
+  <div>
+    <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>
+    <IndexLink activeClassName='active' to='/about'>About</IndexLink>
+    <IndexLink activeClassName='active' to='/namedComponent'>Named Components</IndexLink>
+  </div>
+)
+
+const Container = (props) => (
+  <div>
+    <Nav />
+    {props.children}
+  </div>
+)
+
+const Title = () => <h1>Hello from Title Component</h1>
+
+const SubTitle = () => <h1>Hello from SubTitle Component</h1>
+
+const NamedComponents = (props) => (
+  <div>
+    {props.title}
+    <br/>
+    {props.subTitle}
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Container}>
+          <IndexRoute component={Home} />
+          <Route path='address' component={Address}>
+            <IndexRoute component={TwitterFeed} />
+            <Route path='instagram' component={Instagram} />
+          </Route>
+          <Route path='/namedComponent' component = { NamedComponents }>
+            <IndexRoute components={{ title: Title, subTitle: SubTitle }} />
+          </Route>
+          <Route path='*' component={NotFound} />
+        </Route>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ react-routing/nader-dabit-tutorial/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: path.resolve(__dirname, 'app'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './app',
+    port: 8100
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
